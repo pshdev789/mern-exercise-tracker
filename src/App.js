348eda98b07b8769cc82922e1ab69c6a6f204305
pshdev789 <68139465+pshdev789@users.noreply.github.com>
2020-09-15 09:29:38 +0530

    Added Router modifications
    Added Router.js files and Related paths to the App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-// import './bootstrap/dist/css/bootstrap.min.css';
-import '/bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Route, Router } from 'react-router-dom';
+
+import NavBar from "./components/navbar.component";
+import ExerciseList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component"
+
 
 function App() {
   return (
-    <div className="container">
-      Hello World!
-    </div>
+    <Router>
+      <NavBar/>
+      <br/>
+      <Route path="/" exact component={ExerciseList} />
+      <Route path="/edit/:id" exact component={EditExercise} />
+      <Route path="/create" exact component={CreateExercise} />
+      <Route path="/user" exact component={CreateUser} />
+      {/* <Route path="/" exact component={DeleteExercise} /> */}
+      
+    </Router>
   );
 }
 
