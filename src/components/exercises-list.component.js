b73eb58e52f8a04dde5edbbaaf9b10c1d8ed0ce8
pshdev789 <68139465+pshdev789@users.noreply.github.com>
2020-09-17 17:21:07 +0530

    Update exercises-list.component.js
    Added Exercise componenet as a fucntional component.

--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
+// functional react component
+// Lack of state and lifecycle methods
+const Exercise = props => {
+    <tr>
+        <td>{props.exercise.username}</td>
+        <td>{props.exercise.dscription}</td>
+        <td>{props.exercise.duration}</td>
+        <td>{props.exercise.date.substring(0,10)}</td>
+        <td>
+            <Link to={"/edit/" + props.exercise._id}>edit</Link> | <a href="#" onClick={() => { props.deleteExercise(props.exercise._id) }}>delete</a>
+        </td>
+    </tr>
+
+}
+
+// class component
 export default class ExercisesList extends Component {
 
     constructor(props) {
@@ -17,20 +33,50 @@ export default class ExercisesList extends Component {
 
     }
 
-    componentDidMount(){
+    componentDidMount() {
         axios.get('http://localhost:5000/exercises/')
             .then(response => {
                 this.setState({ exercises: response.data })
             })
-            .catch((err) =>{
+            .catch((err) => {
                 console.log(err);
             });
+    }
+
+    deleteExercise(id) {
+        axios.delete('http://localhost:5000/exercises/' + id)
+            .then(res => console.log(res.data));
+
+        this.setState({
+            exercises: this.state.exercises.filter(el._id !== id)
+        })
+    }
+
+    exerciseList() {
+        return this.state.exercises.map(currentexercise => {
+            return <Exercise exercise={currentexercise} deleteExercise
+                ={this.deleteExercise} key={currentexercise._id} />
+        })
     }
 
     render() {
         return (
             <div>
-                <p>You are in the ExerciseList Component</p>
+                <h3>Logged Exercises</h3>
+                <table className="table">
+                    <thead className="thead-light">
+                        <tr>
+                            <th>Username</th>
+                            <th>Description</th>
+                            <th>Duration</th>
+                            <th>Date</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.ExercisesList()}
+                    </tbody>
+                </table>
             </div>
         );
     }
